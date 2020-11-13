53a48e2c4afcfb6feb7dd81a4e507d438ac37616
eikrt <e.i.korte@gmail.com>
2020-11-13 23:47:57 +0200

    first commit

---
+++ elpa/cider-20201018.1453/cider-pkg.el
@@ -0,0 +1,24 @@
+(define-package "cider" "20201018.1453" "Clojure Interactive Development Environment that Rocks"
+  '((emacs "25")
+    (clojure-mode "5.12")
+    (parseedn "0.2")
+    (pkg-info "0.4")
+    (queue "0.2")
+    (spinner "1.7")
+    (seq "2.16")
+    (sesman "0.3.2"))
+  :commit "2da3b76e8d849a5588ded9abbdca685f636f985f" :keywords
+  '("languages" "clojure" "cider")
+  :authors
+  '(("Tim King" . "kingtim@gmail.com")
+    ("Phil Hagelberg" . "technomancy@gmail.com")
+    ("Bozhidar Batsov" . "bozhidar@batsov.com")
+    ("Artur Malabarba" . "bruce.connor.am@gmail.com")
+    ("Hugo Duncan" . "hugo@hugoduncan.org")
+    ("Steve Purcell" . "steve@sanityinc.com"))
+  :maintainer
+  '("Bozhidar Batsov" . "bozhidar@batsov.com")
+  :url "http://www.github.com/clojure-emacs/cider")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
