a294c15609cb52e3c88c37d93da5eeeb2b5150c3
Eino Korte <e.i.korte@gmail.com>
2023-12-02 12:29:39 +0200

    added restore to tool-bar-mode due to missing

--- init.el
+++ init.el
@@ -254,6 +254,14 @@
 
 ; ui
 
+; restore because something goofed up
+(defun restore-menu-bar()
+  (interactive)
+  (if (fboundp 'scroll-bar-mode) (scroll-bar-mode 1))
+  (if (fboundp 'tool-bar-mode) (tool-bar-mode 1))
+  (if (fboundp 'menu-bar-mode) (menu-bar-mode 1)))
+
+(restore-menu-bar)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
