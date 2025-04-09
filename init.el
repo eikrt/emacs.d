1d376b2ddaaf08a54ed46eb734c053421abdb6ca
Eino Korte <eino.korte@twoday.com>
2025-04-09 10:49:25 +0300

    initial macos commit

--- init.el
+++ init.el
@@ -1,6 +1,7 @@
 ; Theme
 ;
-(load-theme 'dracula t)
+; (load-theme 'dracula t)
+(load-theme 'modus-operandi t)
 ; Enable Evil
 (require 'evil)
 (evil-mode 1)
@@ -25,11 +26,13 @@
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
+; (minimap-mode 1)
 (add-hook 'xref-backend-functions #'dumb-jump-xref-activate)
 (global-set-key "\C-x\C-o" 'dumb-jump-go-prefer-external-other-window);
 (global-set-key "\C-x\C-k" 'xref-pop-marker-stack);
 (global-set-key "\C-x\C-j" 'dumb-jump-go);
 (global-set-key "\C-x\C-k" 'dumb-jump-back);
+(global-evil-matchit-mode 1)
 (setq gc-cons-threshold 100000000) ; 100 mb
 (setq read-process-output-max (* 1024 1024)) ; 1mb
 ; (set-face-attribute 'default nil :font Droid Sans Mono-10 )
@@ -66,7 +69,6 @@
 	         '("melpa" . "https://melpa.org/packages/"))
 
 (package-initialize)
-
 (use-package vertico
   :ensure t
   :init
@@ -82,7 +84,27 @@
 
 (use-package rust-mode)
 
-(use-package lsp-mode)
+(unless (package-installed-p 'lsp-mode)
+  (package-refresh-contents)
+  (package-install 'lsp-mode))
+
+;; Optionally, install lsp-ui for enhanced UI features (e.g., hover documentation)
+(use-package lsp-ui
+  :ensure t
+  :after lsp-mode
+  :hook (lsp-mode . lsp-ui-mode)
+  :config
+  ;; Enable the feature to show documentation on hover
+  (setq lsp-ui-doc-enable t)
+  ;; Enable the feature to show a signature help popup
+  (setq lsp-ui-sideline-enable t)
+  ;; Show diagnostics inline
+  (setq lsp-ui-sideline-show-diagnostics t))
+(setq ring-bell-function 'ignore)
+(use-package company
+  :ensure t
+  :config
+  (add-hook 'after-init-hook 'global-company-mode))
 
 (use-package smartparens
   :ensure t
@@ -90,13 +112,66 @@
   (smartparens-global-mode)
 )
 
+; plugins
+(load "/Users/eino.korte/repo/emacs.d/plugins/apidoc-eww.el")
+
 ; Keybindings  
+(global-set-key (kbd "C-c a") 'apidoc-eww)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-c <left>")  'windmove-left)
+(global-set-key (kbd "C-c <right>") 'windmove-right)
+(global-set-key (kbd "C-c <up>")    'windmove-up)
+(global-set-key (kbd "C-c <down>")  'windmove-down)
+(global-set-key [M-left] 'tabbar-backward-tab)
+(global-set-key [M-right] 'tabbar-forward-tab)
+(global-set-key (kbd "C-c d") #'deadgrep)
 (global-set-key (kbd "C-c t") 'treemacs)
 (global-set-key (kbd "C-c e") 'eval-buffer)
-  ; Dashboard
+(global-set-key (kbd "C-c g") 'google-this)
+(global-set-key (kbd "C-c r") 'golden-ratio)
+; (define-key evil-normal-state-map (kbd "€") 'move-end-of-line)
+(define-key global-map (kbd "s-4") 'move-end-of-line)
+(tab-bar-mode 1)
+(rainbow-mode 1)
+(helm-mode 1)
+(winner-mode 1)
+(golden-ratio 1)
+(global-set-key (kbd "C-c c") 'comment-region)
+(ac-config-default)
+(global-set-key "\M-(" (lambda () (interactive) (insert "{")))
+(global-set-key "\M-)" (lambda () (interactive) (insert "}")))
+(global-set-key "\M-8" (lambda () (interactive) (insert "[")))
+(global-set-key "\M-9" (lambda () (interactive) (insert "]")))
+(global-set-key "\M-<" (lambda () (interactive) (insert "|")))
+(use-package web-mode
+  :ensure t
+  :mode
+  (("\\.phtml\\'" . web-mode)
+   ("\\.php\\'" . web-mode)
+   ("\\.js\\'" . web-mode)
+   ("\\.jsx\\'" . web-mode)
+   ("\\.ts\\'" . web-mode)
+   ("\\.tsx\\'" . web-mode)
+   ("\\.tpl\\'" . web-mode)
+   ("\\.[agj]sp\\'" . web-mode)
+   ("\\.as[cp]x\\'" . web-mode)
+   ("\\.erb\\'" . web-mode)
+   ("\\.mustache\\'" . web-mode)
+   ("\\.djhtml\\'" . web-mode)))
+(setq desktop-restore-forces-onscreen nil)
+(add-hook 'web-mode-hook 'prettier-js-mode)
+(add-hook 'web-mode-hook 'lsp-mode)
+(add-hook 'web-mode-hook 'company-mode)
+(add-hook 'web-mode-hook 'lsp-ui-mode)
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+; Dashboard
+(require 'dimmer)
+ (dimmer-configure-which-key)
+ (dimmer-configure-helm)
+ (dimmer-mode t)
 (set-face-attribute 'default nil
-                    :family "Ubuntu Mono"
-                    :height 110)  ;; Adjust height as needed (110 is 11pt)
+                    :family "Andale Mono"
+                    :height 120)  ;; Adjust height as needed (110 is 11pt)
 ;; Set the title
 (setq dashboard-banner-logo-title "Welcome to Emacs!")
 ; Set the banner
@@ -117,9 +192,9 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages '(org-modern evil dracula-theme)))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
- 
+  '(minimap-active-region-background
+    ((((background dark)) (:background "#40e0d0"))
+      (t (:background "#000000")))
+    "Face for the active region in the minimap.
+By default, this is only a different background color."
+    :group 'minimap))
---
+++ plugins/apidoc-eww.el
@@ -0,0 +1,3 @@
+(defun apidoc-eww ()
+  (interactive)
+  (eww "https://digione-api-test.kuntien.fi/apidoc"))
